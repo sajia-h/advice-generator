a4c305386a35adac829c08d2bf99dd2f82d00908
sajia-h <56345712+sajia-h@users.noreply.github.com>
2022-11-24 22:04:55 +0000

    Advice and ID is fetched from API

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
-//how to fetch advice slip api?
+//query selector
+generateQuote();
 
-fetch('https://api.adviceslip.com/advice')
-;
+const dice = document.querySelector('.button');
+dice.addEventListener('click', generateQuote);
 
-const response = await fetch('https://api.adviceslip.com/advice?t=' + Math.random());
+/* Note: data was arbitrarily named. You can name it whatever you want. 
+data is the value returned from fetch(). */
+
+  function generateQuote() {
+    fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('advice-no').innerHTML = data.slip.id;
+      document.getElementById('quote').innerHTML = data.slip.advice;
+    });
+  }
+
